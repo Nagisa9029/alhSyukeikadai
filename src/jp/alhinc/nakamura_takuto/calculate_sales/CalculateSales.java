428708d0e11eba7d4a57e8f79c4601d224d307b9
nakamura.takuto <nakamura.takuto@m.alhinc.jp>
2018-07-05 18:15:01 +0900

    First commit

--- src/jp/alhinc/nakamura_takuto/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/nakamura_takuto/calculate_sales/CalculateSales.java
@@ -1,5 +1,19 @@
 package jp.alhinc.nakamura_takuto.calculate_sales;
 
-public class CalculateSales {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
+public class CalculateSales {
+	public static void main(String[] args) {
+		File file = new File(args[0], "branch.lst");
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String line = br.readLine();
+		while((line = br.readLine()) != null) {
+			
+		    System.out.println(line);
+		}
+		br.close();
+	}
 }
