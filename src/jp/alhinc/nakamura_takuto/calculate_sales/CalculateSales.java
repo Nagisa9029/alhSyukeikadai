e7a5dda78d5a0adce64add233f6cb7173f8572ed
nakamura.takuto <nakamura.takuto@m.alhinc.jp>
2018-07-09 18:11:16 +0900

    集計コード製作中

--- src/jp/alhinc/nakamura_takuto/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/nakamura_takuto/calculate_sales/CalculateSales.java
@@ -58,18 +58,31 @@ public class CalculateSales {
 		
 		
 		
+		
 		for(int i = 0; i < fileName.size(); i++){
 			System.out.println(fileName.get(i));
+			//int sum = 0 ;
 			try{
 		        File file = new File(args[0], fileName.get(i));
     	        FileReader filereader = new FileReader(file);
                 BufferedReader br = new BufferedReader(filereader);
     	        String str = br.readLine();
+    	        String[] arraySales = new String[2];
+    	        int n = 0;
     	        while(str != null){
     	        	System.out.println(str);
+    	        	arraySales[n] = (str);
     	    		str = br.readLine();
+    	    		n++;
     	    	}
-    	        br.close();	        
+    	        br.close();
+    	        
+    	        if(branchSales.containsKey(arraySales[0])){
+    	        	int sum = Integer.parseInt(arraySales[1] + branchSales.get(arraySales[0]));
+    	        	branchSales.put(arraySales[0], Integer.toString(sum));
+    	        }else{
+    	            branchSales.put(arraySales[0], arraySales[1]);
+    	        }
 			} catch (FileNotFoundException e) {
 				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
@@ -78,5 +91,7 @@ public class CalculateSales {
 				e.printStackTrace();
 			}
 		}
+		
+		System.out.println(branchSales.get("005"));
 	}
 }
