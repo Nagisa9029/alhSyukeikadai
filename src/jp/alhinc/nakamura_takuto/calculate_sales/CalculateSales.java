5cc900ba3b9e00f5609a6b30d04f7f17699c078c
nakamura.takuto <nakamura.takuto@m.alhinc.jp>
2018-07-11 18:01:32 +0900

    Method

--- src/jp/alhinc/nakamura_takuto/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/nakamura_takuto/calculate_sales/CalculateSales.java
@@ -26,6 +26,8 @@ public class CalculateSales {
 			return;
 		}
 /*------------------------支店定義ファイル読み込み--------------------*/
+		inPut(args[0], "branch.lst", branchName);
+/*
 		File file = new File(args[0], "branch.lst");
 		BufferedReader br = null;
 
@@ -62,7 +64,7 @@ public class CalculateSales {
 				return;
 			}
 		}
-
+*/
 
 /*---------------------売上ファイル読み込み--------------------*/
 		ArrayList<String> rcdName = new ArrayList<String>();
@@ -70,6 +72,7 @@ public class CalculateSales {
 
 		File dir = new File(args[0]);
 		File[] files = dir.listFiles();
+		BufferedReader br = null;
 		for(int i = 0; i < files.length; i++){
 			File item = files[i];
 
@@ -154,7 +157,88 @@ public class CalculateSales {
 
 
 /*---------------------出力処理------------------------*/
+
+		outPut(args[0], "branch.out", branchName, branchSales);
+		
+		/*
 		File fileUp = new File(args[0], "branch.out");
+		BufferedWriter bw = null;
+		try {
+			FileWriter fw = new FileWriter(fileUp);
+			bw = new BufferedWriter(fw);
+
+		//branch.outファイルへ出力
+			for(Map.Entry<String, String> entry : branchName.entrySet()) {
+				if(branchSales.get(entry.getKey()) == null){
+					bw.write(entry.getKey() + "," + entry.getValue() + "," + 0);
+					bw.newLine();
+				}else{
+					bw.write(entry.getKey() + "," + entry.getValue() + "," + branchSales.get(entry.getKey()));
+					bw.newLine();
+				}
+			}
+
+		} catch (IOException e) {
+			System.out.println("予期せぬエラーが発生しました");
+			return;
+		}finally{
+			try {
+				if(bw != null){
+					bw.close();
+				}
+			} catch (IOException e) {
+				System.out.println("予期せぬエラーが発生しました");
+				return;
+			}
+		}
+		*/
+	}
+
+/*--------------入力メソッド(支店定義ファイル-------------------------*/
+
+	public static void inPut(String arg, String filename, HashMap<String, String>branchName){
+		File file = new File(arg, filename);
+		BufferedReader br = null;
+
+		//支店定義ファイルが存在しない場合
+		if(!file.exists()){
+			System.out.println("支店定義ファイルが存在しません");
+			return;
+		}
+		try{
+			FileReader fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String s;
+			while((s = br.readLine()) != null){
+				String[] str = s.split(",");
+
+				//支店定義ファイルのフォーマットに関するIF
+				if(str.length == 2 && str[0].matches("^\\d{3}$")){
+					branchName.put(str[0], str[1]);
+				}else{  //支店定義ファイルのフォーマットが不正な場合
+					System.out.println("支店定義ファイルのフォーマットが不正です");
+					return;
+				}
+			}
+		}catch(IOException e) {
+			System.out.println("予期せぬエラーが発生しました");
+			return;
+		}finally{
+			try {
+				if(br != null){
+					br.close();
+				}
+			}catch (IOException e) {
+				System.out.println("予期せぬエラーが発生しました");
+				return;
+			}
+		}
+	}
+
+
+/*---------------------出力メソッド-----------------------------------*/
+	public static void outPut(String arg, String filename, HashMap<String, String>branchName, HashMap<String, Long>branchSales){
+		File fileUp = new File(arg, filename);
 		BufferedWriter bw = null;
 		try {
 			FileWriter fw = new FileWriter(fileUp);
